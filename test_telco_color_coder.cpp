38b856ee9d138a2bbcf09775dc3552be2c621ceb
gautam-sh <gtmsharma70@gmail.com>
2022-06-14 09:43:56 +0530

    remove duplication

--- test_telco_color_coder.cpp
+++ test_telco_color_coder.cpp
@@ -1,6 +1,21 @@
 #include <assert.h>
 #include "telco_color_coder.hpp"
 
+namespace TelCoColorCoder
+{
+    ColorPair GetColorFromPair(int pair) {
+        int zeroBasedPair = pair - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPair / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPair % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int GetPairFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
+
 void testNumberToPair(int pair,
 TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
