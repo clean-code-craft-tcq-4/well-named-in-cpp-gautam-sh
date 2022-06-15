e6eecc4da206e41da2092446d60dc5ac5d4fab65
gautam-sh <gtmsharma70@gmail.com>
2022-06-15 22:22:30 +0530

    update for build

--- telco_color_coding_manual.hpp
+++ telco_color_coding_manual.hpp
@@ -1,11 +1,12 @@
 #include "telco_color_coder.hpp"
 #include <map>
 
-int main() {
+std::map<std::string, int> generateColorCodingManual() {
     std::map<std::string, int> color_coding_manual;
 
     for(int i=1; i <= 25; ++i)
     {
         color_coding_manual[TelCoColorCoder::GetColorFromPair(i).ToString()] = i;
     }
+    return color_coding_manual;
 }
